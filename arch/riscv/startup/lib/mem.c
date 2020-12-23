a7568190ca82b564fce3e7352c35240aafcda54c
Yuri Zaporozhets <r_tty@yahoo.co.uk>
2020-12-24 00:31:31 +0100

    mem.c: use real routines, not GCC's built-in
    If e.g. __builtin_memcpy() is called from memcpy(), in fact it's calling itself (recursively).
    GCC doesn't complain about this. Strange.
    
    Signed-off-by: Yuri Zaporozhets <r_tty@yahoo.co.uk>

--- arch/riscv/startup/lib/mem.c
+++ arch/riscv/startup/lib/mem.c
@@ -1,31 +1,108 @@
 #include <stddef.h>
 
-int memcmp(const void *s1, const void *s2, size_t n)
+void *memset(void *dst, int c, unsigned n)
 {
-    return __builtin_memcmp(s1, s2, n);
+    char *cdst = (char *) dst;
+    int i;
+    for (i = 0; i < n; i++) {
+        cdst[i] = c;
+    }
+    return dst;
 }
 
-size_t strlen(const char *s)
+int memcmp(const void *v1, const void *v2, unsigned n)
 {
-    return __builtin_strlen(s);
+    const unsigned char *s1, *s2;
+
+    s1 = v1;
+    s2 = v2;
+    while (n-- > 0) {
+        if (*s1 != *s2)
+            return *s1 - *s2;
+        s1++, s2++;
+    }
+
+    return 0;
 }
 
-char *strcpy(char *dest, const char *src)
+void *memmove(void *dst, const void *src, unsigned n)
 {
-    return __builtin_strcpy(dest, src);
+    const char *s;
+    char *d;
+
+    s = src;
+    d = dst;
+    if (s < d && s + n > d) {
+        s += n;
+        d += n;
+        while (n-- > 0)
+            *--d = *--s;
+    } else
+        while (n-- > 0)
+            *d++ = *s++;
+
+    return dst;
 }
 
-int strcmp(const char *s1, const char *s2)
+// memcpy exists to placate GCC.  Use memmove.
+void *memcpy(void *dst, const void *src, unsigned n)
 {
-    return __builtin_strcmp(s1, s2);
+    return memmove(dst, src, n);
 }
 
-void *memset(void *s, int c, size_t n)
+int strncmp(const char *p, const char *q, unsigned n)
 {
-    return __builtin_memset(s, c, n);
+    while (n > 0 && *p && *p == *q)
+        n--, p++, q++;
+    if (n == 0)
+        return 0;
+    return (unsigned char) * p - (unsigned char) * q;
 }
 
-void *memmove(void *dest, const void *src, size_t n)
+char *strncpy(char *s, const char *t, int n)
 {
-    return __builtin_memmove(dest, src, n);
+    char *os;
+
+    os = s;
+    while (n-- > 0 && (*s++ = *t++) != 0);
+    while (n-- > 0)
+        *s++ = 0;
+    return os;
+}
+
+// Like strncpy but guaranteed to NUL-terminate.
+char *safestrcpy(char *s, const char *t, int n)
+{
+    char *os;
+
+    os = s;
+    if (n <= 0)
+        return os;
+    while (--n > 0 && (*s++ = *t++) != 0);
+    *s = 0;
+    return os;
+}
+
+int strlen(const char *s)
+{
+    int n;
+
+    for (n = 0; s[n]; n++);
+    return n;
+}
+
+int strcmp(const char *p, const char *q)
+{
+    while(*p && *p == *q)
+        p++, q++;
+    return (unsigned char)*p - (unsigned char)*q;
+}
+
+char *strcpy(char *s, const char *t)
+{
+    char *os;
+
+    os = s;
+    while((*s++ = *t++) != 0) ;
+    return os;
 }
