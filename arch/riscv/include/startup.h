516f111decf0c22ce05ad8cc70a61773c871f693
Yuri Zaporozhets <r_tty@yahoo.co.uk>
2020-12-30 00:49:15 +0100

    riscv: added a file startup.h, which includes the others from include/kernel (with definitions and prototypes)
    Signed-off-by: Yuri Zaporozhets <r_tty@yahoo.co.uk>

---
+++ arch/riscv/include/startup.h
@@ -0,0 +1,18 @@
+/**
+ * \file board.c
+ *
+ * Architecture-specific startup definitions.
+ *
+ * \author Yuri Zaporozhets <r_tty@yahoo.co.uk>
+ *
+ * \license SPDX-License-Identifier: BSD-2-Clause
+ */
+
+#ifndef _RISCV_STARTUP_H
+#define _RISCV_STARTUP_H
+
+#include <kernel/startup.h>
+#include <kernel/startup_proto.h>
+#include <asm/image.h>
+
+#endif
