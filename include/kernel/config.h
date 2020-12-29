c8b27040df5b413041245f48bd044fb88f1f6007
Yuri Zaporozhets <r_tty@yahoo.co.uk>
2020-12-30 00:42:58 +0100

    config.h: initial version
    Signed-off-by: Yuri Zaporozhets <r_tty@yahoo.co.uk>

---
+++ include/kernel/config.h
@@ -0,0 +1,12 @@
+/*
+ * Kernel and startup configuration file.
+ */
+
+#define CONFIG_ULTRA_VERBOSE
+
+#define CONFIG_KPRINTF_MAX_LIMIT        256
+
+#define CONFIG_KPRINTF_PTR_BOUND_LOW    0x80000000
+#define CONFIG_KPRINTF_PTR_BOUND_HIGH   0x8FFFFFFF
+
+#define CONFIG_MAX_BOOTLINE_SIZE        256
