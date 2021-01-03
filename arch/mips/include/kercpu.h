7c81e517567431b1dc01150b7dde0aad0aa3390a
Yuri Zaporozhets <r_tty@yahoo.co.uk>
2021-01-03 18:47:24 +0100

    mips, riscv: updated (or added) kercpu.h

--- arch/mips/include/kercpu.h
+++ arch/mips/include/kercpu.h
@@ -15,32 +15,35 @@
  * $
  */
 
-#include <stdbool.h>
-#include <sys/types.h>
 #include <arch/cpu_def.h>
 
 #define xfer_setjmp(_env)			_setjmp(_env)
 #define xfer_longjmp(_env, _ret, _regs)		_longjmp(_env, _ret)
 
 /*
- * This makes each argument in a kernel call structure take up 64 bits so
- * that it aligns with the register save area layout.
+	This makes each argument in a kernel call structure take up 64 bits so
+	that it aligns with the register save area layout.
 */
 #define XFILLER( line )		uint32_t	filler##line
 #define FILLER( line )		XFILLER( line )
 
-#define KARGSLOT( arg )		arg; FILLER(__LINE__)
+#if defined(__BIGENDIAN__)
+	#define KARGSLOT( arg )		FILLER(__LINE__); arg
+#elif defined(__LITTLEENDIAN__)
+	#define KARGSLOT( arg )		arg; FILLER(__LINE__)
+#else
+	#error ENDIAN Not defined for system
+#endif
 
-#define REGIP(reg)		(reg->pc)
-#define REGSP(reg)		(reg->gpr[RISCV_REG_SP])
-#define SETREGIP(reg,v)		(REGIP(reg) = (uintptr_t)(v))
-#define SETREGSP(reg,v)		(REGSP(reg) = (uintptr_t)(v))
+#define REGTYPE(rs)		((rs)->regs[MIPS_CREG(MIPS_REG_V0)])
+#define REGSTATUS(rs)		((rs)->regs[MIPS_CREG(MIPS_REG_V0)])
+#define REGIP(rs)		((rs)->regs[MIPS_CREG(MIPS_REG_EPC)])
+#define REGSP(rs)		((rs)->regs[MIPS_CREG(MIPS_REG_SP)])
 
-#define REGSTATUS(reg)		((reg)->gpr[RISCV_REG_X0])
-#define REGTYPE(reg)		((reg)->gpr[RISCV_REG_X0])
-#define SETREGSTATUS(reg,v)	(REGSTATUS(reg) = (uintptr_t)(v))
-#define SETREGTYPE(reg,v)	(REGTYPE(reg) = (uintptr_t)(v))
-
+#define SETREGTYPE(rs,v)	MIPS_REG_SETx64(rs, MIPS_REG_V0, v)
+#define SETREGSTATUS(rs,v)	MIPS_REG_SETx64(rs, MIPS_REG_V0, v)
+#define SETREGIP(rs,v)		MIPS_REG_SETx64(rs, MIPS_REG_EPC, v)
+#define SETREGSP(rs,v)		MIPS_REG_SETx64(rs, MIPS_REG_SP, v)
 
 #define SETKIP_FUNC(thp,v)	cpu_invoke_func(thp, (uintptr_t)v)
 
@@ -48,11 +51,12 @@
 #define KERERR_SKIPAHEAD	8	/* increment IP by this much on error */
 
 /*
- * Convert physical address to pointer value and vice-versa when in a
+ * Convert physical address to pointer value and vis-versa when in a
  * physical memory model.
  */
-#define PHYS_TO_PTR(phys)		((void *)RISCV_PHYS_TO_KSEG0(phys))
-#define PTR_TO_PHYS(ptr)		((uintptr_t)RISCV_KSEG0_TO_PHYS(ptr))
+#define PHYS_TO_PTR(phys)		((void *)MIPS_PHYS_TO_KSEG0(phys))
+#define PTR_TO_PHYS(ptr)		((uintptr_t)MIPS_KSEG0_TO_PHYS(ptr))
+
 
 #define rd_probe_1(ptr)	({ __attribute__((unused)) uint32_t dummy = *(const volatile uint32_t *)(ptr); })
 
@@ -112,22 +116,19 @@ wr_probe_num(void *loc, int num) {
 
 #if defined(VARIANT_smp)
 
-#define am_inkernel am_inkernel
-static inline bool am_inkernel(void)
-{
-    uintptr_t highmem;
-    __asm__ ("ld %0, pc; srli %0, %0, 32" : "=r" (highmem));
-    return highmem;
-}
-
-#define bitset_inkernel(bits)	atomic_set(&inkernel, bits)
-#define bitclr_inkernel(bits)	atomic_clr(&inkernel, bits)
+	#define am_inkernel() \
+		({	uintptr_t sp; \
+			__asm__ ("move %0, $sp" : "=r" (sp)); \
+			((sp >= ker_stack_bot) && (sp < ker_stack_top)); \
+		})
+	#define bitset_inkernel(bits)	atomic_set(&inkernel, bits)
+	#define bitclr_inkernel(bits)	atomic_clr(&inkernel, bits)
 
 #endif /* SMP */
 
-#if !defined(RISCV_CAUSE2SIGMAP_CONST)
-#define RISCV_CAUSE2SIGMAP_CONST const
+#if !defined(MIPS_CAUSE2SIGMAP_CONST)
+#define MIPS_CAUSE2SIGMAP_CONST const
 #endif
-extern RISCV_CAUSE2SIGMAP_CONST unsigned long __mips_cause2sig_map[];
+extern MIPS_CAUSE2SIGMAP_CONST unsigned long __mips_cause2sig_map[];
 
 extern uintptr_t	next_instruction(CPU_REGISTERS *ctx);
